895b7d01173ca04f298f0e2bb83fe3b231ce9805
Rosenheim <patrickrosenheim@gmail.com>
2016-04-07 15:14:45 +0200

    It fucking works...

--- patrick.ino
+++ patrick.ino
@@ -3,7 +3,7 @@
 const uint8_t RED_PIN = 9;
 const uint8_t YEL_PIN = 8;
 const uint8_t GRN_PIN = 7;
-//
+// Second set
 const uint8_t RED2_PIN = 5;
 const uint8_t YEL2_PIN = 3;
 const uint8_t GRN2_PIN = 6;
@@ -12,65 +12,89 @@ const uint32_t orangeWait = 500;
 const uint32_t greenWait = 2000;
 const uint32_t yellowWait = 1000;
 
+long time = 0;
 bool is1going = false;
 bool is2going = false;
-int count = 0;
+int redToOrange = 3000;
+int orangeToGreen = 1000;
+int greenToYellow = 3000;
+int yellowToRed = 500;
+
+int maxWaitTime = 
+    redToOrange +
+    orangeToGreen +
+    greenToYellow +
+    yellowToRed;
 
 SemaphoreHandle_t sem;
 
 static void Thread1(void* arg) {
   while (1) {
-    // Wait for signal.
+    // Wait for signal from 2.
     xSemaphoreTake(sem, portMAX_DELAY);
+    // If 2 is still running, wait.
+    while ( is2going ) {
+      Serial.println("1: Waiting for 2.");
+    }
 
     is1going = true;
-  	Serial.println("1: Start");
+  	//Serial.println("1: Start");
     // Turn LED off.
-	red(2);
-	vTaskDelay(1000);
-	orange(2);
-	vTaskDelay(600);
-	green(2);
-	vTaskDelay(1500);
-	yellow(2);
-	vTaskDelay(600);
-	red(2);
-	is1going = false;
-	if ( is1going ) {
-		// Send signal.
-   		xSemaphoreGive(sem);
-	}
-
-	Serial.println("1: End");
+  	red(1);
+  	vTaskDelay(redToOrange);
+  	orange(1);
+    vTaskDelay(orangeToGreen);
+    green(1);
+    vTaskDelay(greenToYellow);
+    yellow(1);
+    vTaskDelay(yellowToRed);
+    red(1);
+    vTaskDelay(20);
+    is1going = false;
+    //Serial.println("1: End");
+  	if ( ! is1going ) {
+      // Send signal.
+      //Serial.println("1: Ping.");
+      xSemaphoreGive(sem);
+    }
+    // Wait for signal from 2.
+    xSemaphoreTake(sem, maxWaitTime);
   }
 }
-
 static void Thread2(void* arg) {
-  while(1) {
-
-    // Send signal.
-    xSemaphoreGive(sem);
+  while (1) {
+    // If 2 is still running, wait.
+    while ( is1going ) {
+      Serial.println("2: Waiting for 1.");
+    }
 
     is2going = true;
-  	Serial.println("2: Start");
-
-    // Turn LED on.
-    red(1);
-	vTaskDelay(1000);
-	orange(1);
-	vTaskDelay(600);
-	green(1);
-	vTaskDelay(1500);
-	yellow(1);
-	vTaskDelay(600);
-	red(1);
-
-	is2going = false;
+    Serial.println("2: Start");
+    // Turn LED off.
+    red(2);
+    vTaskDelay(redToOrange);
+    orange(2);
+    vTaskDelay(orangeToGreen);
+    green(2);
+    vTaskDelay(greenToYellow);
+    yellow(2);
+    vTaskDelay(yellowToRed);
+    red(2);
+    vTaskDelay(20);
+    is2going = false;
     Serial.println("2: End");
+    if ( ! is2going ) {
+      // Send signal.
+      // Serial.println("2: Ping.");
+      xSemaphoreGive(sem);
+    }
+    // Wait for signal from 2.
+    xSemaphoreTake(sem, maxWaitTime);
   }
 }
 
 void setup() {
+  time = millis();
   Serial.begin(9600);
   pinMode(RED_PIN, OUTPUT);
   pinMode(YEL_PIN, OUTPUT);
@@ -99,7 +123,10 @@ void setup() {
 }
 
 void loop() {
-	buttonState = digitalRead(2);
+  // time = millis();
+  // if ( time % 1000 == 0 && time > 0) {
+  //   Serial.println(millis());
+  // }
 }
 
 void red(int i) {
